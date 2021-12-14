a698741e7168603741a116c47c6179236c09c276
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-12-13 21:28:07 -0300

    TypeScript: evoluindo o meu JavaScript

---
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -0,0 +1,14 @@
+export class Negociacao {
+    
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor){
+
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor; 
+    }
+
+}
