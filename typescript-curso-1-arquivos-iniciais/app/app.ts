8d381205501bdb69ab528f1d9e1e0230a5bfb569
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-12-16 19:35:18 -0300

    TypeScript: arquivos ts,  configurações do compilador, arquivo tsconfig.json, aprimorando as configurações, automatizando a compilação de arquivos

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,6 +1,10 @@
 import { Negociacao } from './models/negociacao.js';
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
+
 alert("hello Dani");
+
+
+
 /* negociacao.quantidade = 1000;
-console.log(negociacao);  */ 
+console.log(negociacao);  */
