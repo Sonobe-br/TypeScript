93f12aa8ac410c7cdc922879a4276b70875aa49f
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-01 14:23:04 -0300

    utilização de Generics, encapsulamento, solucionando questões de mutabilidade, aplicando o tipo ReadonlyArray, adicionando negociações na lista.

--- typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
+++ typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
@@ -1,10 +1,12 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
 
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes: Negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -16,8 +18,9 @@ export class NegociacaoController {
     adicona(): void {
         
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
-
+        this.negociacoes.adiciona(negociacao);
+        /* this.negociacoes.lista().pop(); */
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
         
     }
--- typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
@@ -4,4 +4,16 @@ export class Negociacoes {
 
     private negociacoes: Array<Negociacao> = [];
 
+    adiciona(negociacao:Negociacao) {
+
+        this.negociacoes.push(negociacao);
+
+    }
+
+    lista(): ReadonlyArray<Negociacao> {
+
+        return this.negociacoes;      
+
+    }
+
 }
