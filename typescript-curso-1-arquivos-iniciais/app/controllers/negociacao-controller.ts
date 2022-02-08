da4070efeafe16da7d84cc7a3e3a2a435c90b7cb
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-02-08 13:41:29 -0300

    trabalhando com enuns

--- typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
+++ typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/diasDaSemana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -11,10 +12,7 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView'); 
-    private readonly SABADO = 6;
-    private readonly DOMINGO = 0;
     
-
     constructor() {
 
         this.inputData = document.querySelector('#data');
@@ -43,7 +41,8 @@ export class NegociacaoController {
 
     private diasUteis(data: Date) {
 
-        return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO;
+        return data.getDay() > DiasDaSemana.DOMINGO 
+            && data.getDay() < DiasDaSemana.SABADO;
 
     }
     
