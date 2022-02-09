d38ec015b04f62517ced84657ca474b5413113b5
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-02-09 12:10:30 -0300

    StrictNullChecks

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -3,12 +3,20 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', event => {
+if(form) {
     
-    event.preventDefault();
-    controller.adiciona();
-    
-})
+    form.addEventListener('submit', event => {
+        
+        event.preventDefault();
+        controller.adiciona();
+        
+    });
+
+} else {
+
+    throw Error ('Não foi possível inicializar a aplicação. Verifique a existencia do form')
+
+}
 
 
 
